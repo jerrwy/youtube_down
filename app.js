14673e659651935797e556af4a6d6b715b883164
xiang.wang <xiang.wang@yintech.cn>
2018-02-08 10:02:31 +0800

    add express api

--- app.js
+++ app.js
@@ -11,12 +11,27 @@
 const crawler = require('./util/youtube_crawler')
 
 
-async function work() {
-  let data = await crawler('cat')
+// async function work() {
+//   let data = await crawler('cat')
   
-  console.log(data)
-}
+//   console.log(data)
+// }
 
 
 
-work()
+// work()
+
+
+const express = require('express')
+const app = express()
+
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+app.get('/search', async (req, res) => {
+  let data = await crawler(req.query.q || 'cat')
+  res.send(data)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
