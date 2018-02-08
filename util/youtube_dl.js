671c773de2d3e5a78906571333d52cb0fee63d42
xiang.wang <xiang.wang@yintech.cn>
2018-02-08 10:39:05 +0800

    fix err

--- util/youtube_dl.js
+++ util/youtube_dl.js
@@ -3,7 +3,7 @@ let process = require('child_process')
 module.exports = {
   downVideo: async (url) => {
     return new Promise((resolve,reject) =>{
-      process.exec(`cd /root/youtube & youtube-dl -f 22 ${url}`, (err, stdout, stderr) => {
+      process.exec(`cd /root/youtube ; youtube-dl -f 22 ${url}`, (err, stdout, stderr) => {
         if (err !== null) reject(err)
         console.log(stdout)
         resolve()
