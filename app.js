3578ff9818300599db9b99eb52f8868112c5ea5c
xiang.wang <xiang.wang@yintech.cn>
2018-02-12 16:10:13 +0800

    fix err for require cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(require('cors'))
+app.use(require('cors')())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
